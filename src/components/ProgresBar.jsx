eeb5c96474c05200469e5146e3af65aee7617bcb
amanharshargu <aman.choubey@argusoft.in>
2024-09-10 13:03:00 +0530

    Initial Commit

---
+++ src/components/ProgresBar.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export default function ProgresBar({timer}){
+    const [remainingTime,setRemainingTime]=useState(timer);
+    useEffect(()=>{
+      const interval=setInterval(()=>{
+        setRemainingTime(prevTime=>prevTime-10)
+      },10);
+      return()=>{clearTimeout(interval)}
+    },[])
+    return <progress value={remainingTime} max={timer}/>
+}
